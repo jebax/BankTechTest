c977003418adf90df062c851ac0a37884829a3ba
jebax <joel.eliot.baxter@gmail.com>
2018-12-03 14:40:14 +0000

    Statement correctly displays withdrawals

--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -25,4 +25,15 @@ describe Statement do
 
     expect(statement.display).to eq "#{headers}\n#{deposit_line}"
   end
+
+  it 'displays a correctly formatted statement for deposit then withdrawal' do
+    withdrawal = double(:transaction, data: [time, nil, 1000.0, 1000.0])
+    withdrawal_line = "#{formatted_time} || || 1000.00 || " \
+    "1000.00"
+
+    transaction_list.transactions << withdrawal
+    statement = described_class.new(transaction_list)
+
+    expect(statement.display).to eq "#{headers}\n#{withdrawal_line}"
+  end
 end
