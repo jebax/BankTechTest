652dd9a5523c99be86e94fe75b47bc55e07c4c77
jebax <joel.eliot.baxter@gmail.com>
2018-12-03 12:45:54 +0000

    correctly formats statement after transaction

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,6 +1,9 @@
 class BankAccount
+  HEADERS = "date || credit || debit || balance"
+
   def initialize
-    @balance = 0
+    @balance = 0.0
+    @transactions = []
   end
 
   def balance
@@ -9,6 +12,7 @@ class BankAccount
 
   def deposit(amount)
     @balance += amount
+    @transactions << [Time.now, nil, amount.to_f, @balance]
   end
 
   def withdraw(amount)
@@ -16,6 +20,20 @@ class BankAccount
   end
 
   def view_statement
-    'date || credit || debit || balance'
+    "#{HEADERS}\n#{@transactions.map { |transaction| format_transaction(transaction) }.join("\n")}"
+  end
+
+  private
+
+  def format_transaction(transaction)
+   formatted = transaction.map do |section|
+      if section.is_a? Float
+        sprintf('%.2f', section)
+      elsif section.is_a? Time
+        section.strftime('%d/%m/%Y')
+      end
+    end
+
+    formatted.join(' || ').sub('  ', ' ')
   end
 end
--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -3,12 +3,6 @@ require 'bank_account'
 describe BankAccount do
   let(:headers) { 'date || credit || debit || balance' }
 
-  context 'account is empty' do
-    it 'shows an empty statement' do
-      expect(subject.view_statement).to eq 'date || credit || debit || balance'
-    end
-  end
-
   context 'account with a positive balance' do
     before(:each) do
       subject.deposit(1000)
@@ -22,6 +16,27 @@ describe BankAccount do
       withdrawal = 500
       subject.withdraw(withdrawal)
       expect(subject.balance).to eq 1000 - withdrawal
+    end
+  end
+
+  describe '#view_statement' do
+    before do
+      @headers = 'date || credit || debit || balance'
+      @time = Time.now
+      @format_time = @time.strftime('%d/%m/%Y')
+      allow(Time).to receive(:now).and_return(@time)
+    end
+
+    it 'shows an empty statement when no transactions are made' do
+      expect(subject.view_statement).to eq @headers + "\n"
+    end
+
+    it 'shows a deposit transaction on a statement with the correct time' do
+      amount = 1000
+      statement = "#{@headers}\n#{@format_time} || || #{amount}.00 || #{amount}.00"
+
+      subject.deposit(amount)
+      expect(subject.view_statement).to eq statement
     end
   end
 end
