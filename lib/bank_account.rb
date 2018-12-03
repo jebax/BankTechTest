e890f0d1a60f0b6d384f2317f56bae33b1fc7cee
jebax <joel.eliot.baxter@gmail.com>
2018-12-03 11:54:04 +0000

    deposit funds increases balance correctly

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,4 +1,16 @@
 class BankAccount
+  def initialize
+    @balance = 0
+  end
+
+  def balance
+    @balance
+  end
+
+  def deposit(amount)
+    @balance += amount
+  end
+  
   def view_statement
     'date || credit || debit || balance'
   end
--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -1,7 +1,19 @@
 require 'bank_account'
 
 describe BankAccount do
-  it 'prints an empty statement when no transactions are made' do
-    expect(subject.view_statement).to eq 'date || credit || debit || balance'
+  let(:headers) { 'date || credit || debit || balance' }
+
+  context 'account is empty' do
+    it 'shows an empty statement' do
+      expect(subject.view_statement).to eq 'date || credit || debit || balance'
+    end
+  end
+
+  context 'one deposit made to account' do
+    it 'updates the balance correctly' do
+      amount = 1000
+      subject.deposit(amount)
+      expect(subject.balance).to eq amount
+    end
   end
 end
