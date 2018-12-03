7b1c86c0ac46c0661083d3da8e938f236638e359
jebax <joel.eliot.baxter@gmail.com>
2018-12-03 16:01:11 +0000

    added high-level feature test

---
+++ spec/features/using_account_spec.rb
@@ -0,0 +1,26 @@
+require 'bank_account'
+require 'statement'
+require 'transaction_list'
+require 'transaction'
+
+feature 'using a bank account' do
+  let(:account) { BankAccount.new }
+  let(:time) { Time.now }
+  let(:formatted_time) { time.strftime('%d/%m/%Y') }
+
+  before do
+    allow(Time).to receive(:now).and_return(time)
+  end
+
+  scenario 'adds 1000, adds 2000, withdraws 500, then views statement' do
+    statement = "date || credit || debit || balance\n#{formatted_time} || " \
+    "1000.00 || || 1000.00\n#{formatted_time} || 2000.00 || || 3000.00\n" \
+    "#{formatted_time} || || 500.00 || 2500.00"
+
+    account.deposit(1000)
+    account.deposit(2000)
+    account.withdraw(500)
+
+    expect(account.view_statement).to eq statement
+  end
+end
