2ab3aefe67936e6dc0e4956ebb35dae050f66910
jebax <joel.eliot.baxter@gmail.com>
2018-12-03 15:31:35 +0000

    transaction instantiates with correct info

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,2 +1,9 @@
 class Transaction
+  attr_reader :deposit, :withdrawal, :balance, :time
+  def initialize(deposit:, withdrawal:, balance:)
+    @deposit = deposit
+    @withdrawal = withdrawal
+    @balance = balance
+    @time = Time.now
+  end
 end
--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -1,5 +1,17 @@
 require 'transaction'
 
 describe Transaction do
-  
+  let(:time) { Time.now }
+
+  before do
+    allow(Time).to receive(:now).and_return(time)
+  end
+
+  it 'is instantiated with the correct information' do
+    transaction = described_class.new(deposit: 1000, withdrawal: nil, balance: 1000)
+    expect(transaction.deposit).to eq 1000
+    expect(transaction.withdrawal).to eq nil
+    expect(transaction.balance).to eq 1000
+    expect(transaction.time).to eq time
+  end
 end
