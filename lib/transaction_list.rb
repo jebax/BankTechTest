3daa8791cbb3ef2180ccd9251e5636b59b09d69c
jebax <joel.eliot.baxter@gmail.com>
2018-12-03 15:24:26 +0000

    TransactionList has add_transaction method
    Creates new instance of its transaction_class

--- lib/transaction_list.rb
+++ lib/transaction_list.rb
@@ -1,9 +1,16 @@
 class TransactionList
-  def initialize
+  def initialize(transaction_class)
+    @transaction_class = transaction_class
     @transactions = []
   end
 
   def transactions
     @transactions.dup
+  end
+
+  def add_transaction(deposit:, withdrawal:, balance:)
+    @transactions << @transaction_class.new(
+      deposit: deposit, withdrawal: withdrawal, balance: balance
+    )
   end
 end
--- spec/transaction_list_spec.rb
+++ spec/transaction_list_spec.rb
@@ -1,7 +1,15 @@
 require 'transaction_list'
 
 describe TransactionList do
+  let(:transaction_class) { double(:transaction_class) }
+  subject { TransactionList.new(transaction_class) }
+
   it 'has a list of transactions' do
     expect(subject.transactions).to eq []
+  end
+
+  it 'can create a new transaction object' do
+    expect(transaction_class).to receive(:new).with(deposit: 1000, withdrawal: nil, balance: 1000)
+    subject.add_transaction(deposit: 1000, withdrawal: nil, balance: 1000)
   end
 end
